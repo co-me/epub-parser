80bb5578ef8e7c4a185beec74704e661beb895fe
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-17 09:10:20 +0900

    Add OCF::Container#to_xml

---
+++ test/test_ocf.rb
@@ -0,0 +1,25 @@
+require_relative 'helper'
+
+class TestOCF < Test::Unit::TestCase
+  class TestContainer < TestOCF
+    def setup
+      @container = EPUB::OCF::Container.new
+      rootfile = EPUB::OCF::Container::Rootfile.new
+      rootfile.full_path = 'OPS/contents.opf'
+      rootfile.media_type = 'application/oebps-package+xml'
+      @container.rootfiles << rootfile
+    end
+
+    def test_container
+      expected = Nokogiri.XML(<<EOC)
+<?xml version="1.0" encoding="UTF-8"?>
+<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
+  <rootfiles>
+    <rootfile full-path="OPS/contents.opf" media-type="application/oebps-package+xml" />
+  </rootfiles>
+</container>
+EOC
+      assert_equal expected.to_s, Nokogiri.XML(@container.to_xml).to_s
+    end
+  end
+end
